bd0aa05d84d3e388eea517653228ab9b6266274c
TimethyH <timethy.hyman@hotmail.com>
2025-08-19 14:51:37 +0200

    CI Uploads generated bindings

--- crates/nvngx-sys/build.rs
+++ crates/nvngx-sys/build.rs
@@ -77,9 +77,7 @@ fn generate_bindings(header: &str) -> bindgen::Builder {
         .bitfield_enum("NVSDK_NGX_DLSS_Feature_Flags")
         .disable_name_namespacing()
         .disable_nested_struct_naming()
-        .default_enum_style(bindgen::EnumVariation::Rust {
-            non_exhaustive: true,
-        })
+        .default_enum_style(bindgen::EnumVariation::Consts)
 }
 
 fn compile_general() {
