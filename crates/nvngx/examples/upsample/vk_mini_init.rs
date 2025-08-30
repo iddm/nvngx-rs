9101911d164a229ab7c3f497d0302fe773c9e18f
EdoardoLuciani <edoardo.luciani@gmail.com>
2025-08-30 23:48:16 +0100

    Add allocator

--- crates/nvngx/examples/upsample/vk_mini_init.rs
+++ crates/nvngx/examples/upsample/vk_mini_init.rs
@@ -1,6 +1,6 @@
-use std::ffi::{c_char, CStr, CString};
-
 use ash::vk;
+use gpu_allocator::vulkan::Allocator;
+use std::ffi::{c_char, CStr, CString};
 
 pub struct VkMiniInit {
     pub entry_fn: ash::Entry,
@@ -52,14 +52,22 @@ impl VkMiniInit {
                 let settings = [x1, x2, x3, x4];
                 let mut pnext = vk::LayerSettingsCreateInfoEXT::default().settings(&settings);
 
-                let instance =
-                    Self::create_instance(&entry_fn, &mut pnext, &instance_extensions, &layer_names);
+                let instance = Self::create_instance(
+                    &entry_fn,
+                    &mut pnext,
+                    &instance_extensions,
+                    &layer_names,
+                );
                 (layer_names, instance)
             } else {
                 let layer_names: Vec<String> = Vec::new();
                 let mut pnext = vk::LayerSettingsCreateInfoEXT::default();
-                let instance =
-                    Self::create_instance(&entry_fn, &mut pnext, &instance_extensions, &layer_names);
+                let instance = Self::create_instance(
+                    &entry_fn,
+                    &mut pnext,
+                    &instance_extensions,
+                    &layer_names,
+                );
                 (layer_names, instance)
             }
         };
@@ -94,7 +102,7 @@ impl VkMiniInit {
 
                     match instance.create_device(physical_device, &device_create_info, None) {
                         Ok(device) => Some((physical_device, device)),
-                        Err(e) =>  None
+                        Err(e) => None,
                     }
                 })
                 .expect("Could not find suitable physical device")
@@ -108,6 +116,18 @@ impl VkMiniInit {
             queue: unsafe { device.get_device_queue(0, 0) },
             device,
         }
+    }
+
+    pub fn get_allocator(&self) -> Allocator {
+        Allocator::new(&gpu_allocator::vulkan::AllocatorCreateDesc {
+            instance: self.instance.clone(),
+            device: self.device.clone(),
+            physical_device: self.physical_device,
+            debug_settings: Default::default(),
+            buffer_device_address: false,
+            allocation_sizes: Default::default(),
+        })
+        .unwrap()
     }
 
     fn create_instance<T: vk::ExtendsInstanceCreateInfo + ?Sized>(
@@ -148,8 +168,11 @@ impl VkMiniInit {
     }
 
     fn get_cptr_vec_from_str_slice(input: &[String]) -> (Vec<*const c_char>, Vec<CString>) {
-        let input_cstr_vec: Vec<CString> =
-            input.iter().cloned().map(|s| CString::new(s).unwrap()).collect();
+        let input_cstr_vec: Vec<CString> = input
+            .iter()
+            .cloned()
+            .map(|s| CString::new(s).unwrap())
+            .collect();
         let input_cptr_vec = input_cstr_vec.iter().map(|s| s.as_ptr()).collect();
         (input_cptr_vec, input_cstr_vec)
     }
